9a89659a49d36e0a53859e8eef19c32cb27fb6ae
jjumonteiro <124930850+jjumonteiro@users.noreply.github.com>
2023-03-11 16:58:13 -0300

    Add files via upload

---
+++ Letrinhas.py
@@ -0,0 +1,13 @@
+letra = str(input("Informe sua letra:"))
+if (letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"):
+     print("Sua letra é uma vogal.")
+
+elif (letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U"):
+    print( "letra" + " é vogal")
+
+else:
+    print("Sua letra é uma consoante.")
+
+
+
+# py .\Letrinhas.py
